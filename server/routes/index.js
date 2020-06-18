5047e0a5af4376454a6ec6223b92fe94601c32c7
Micke <michael.zetterholm@gmail.com>
2020-06-18 08:14:42 +0200

    client / server split

---
+++ server/routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// // middleware that is specific to this router
+// router.use(function timeLog(req, res, next) {
+//   console.log("Time: ", Date.now());
+//   next();
+// });
+
+// define the home page route
+router.use("/users", (req, res) => {
+  res.send('users page');
+});
+
+router.get("/users", (req, res) => {
+  res.send('all users')
+})
+
+router.get('/users/:id', (req, res) => {
+  res.send('one user')
+})
+
+module.exports = router;

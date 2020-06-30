2a3bfb0cdd481e3f29101cd2ee1a58dc28bd1caa
Micke <michael.zetterholm@gmail.com>
2020-06-30 14:54:16 +0200

    mongoose and env

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 
-const app = express();
-const port = 3000;
+const port = 4040;
 const router = require("./routes");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/api", router);
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+const mongohost =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_DEV_HOST
+    : process.env.MONGO_DEV_PROD;
+
+mongoose.connect(mongohost, { useNewUrlParser: true }).then(() => {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/", router);
+
+  app.listen(port, () => {
+    console.log("Server has started!");
+  });
+});

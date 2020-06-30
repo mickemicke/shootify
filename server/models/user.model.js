ec0bad763a295d417f493275aebca45d42256fa7
Micke <michael.zetterholm@gmail.com>
2020-06-30 14:54:20 +0200

    user model

---
+++ server/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: String,
+});
+
+module.exports = mongoose.model("Users", UserSchema);
